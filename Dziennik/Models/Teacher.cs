4ae70958906c232ed43beb98379706897c31e471
quantic92 <mati9.7.1992@wp.pl>
2014-12-03 13:47:55 +0100

    Modele : Parent,Teacher,Student

---
+++ Dziennik/Models/Teacher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dziennik.Models
+{
+    public class Teacher
+    {
+        public int ParentID { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirthday { get; set; }
+
+        public string Phone { get; set; }
+        public string MobilePhone { get; set; }
+        public DateTime DateOfJoin { get; set; }
+        public Boolean Status { get; set; }
+        public DateTime LastLoginDate { get; set; }
+    }
+}
---
+++ Dziennik/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Dziennik.Startup))]
+namespace Dziennik
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
